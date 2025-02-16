7d8e8f6295a5be44d4311c1d64a20d95301a8d33
Agentrb <iypayus796@mailzy.org>
2025-02-16 00:15:26 -0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,9 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx, lua}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx,lua}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx,lua}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx,lua}",
   ],
   theme: {
     extend: {
